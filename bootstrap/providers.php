d7889bea9a8c806bfdeaa6ac88f9294879f129b1
Tim Kleyersburg <tk@wacg.de>
2024-12-12 18:22:18 +0100

    feat: activate refresh database trait in pest feature tests, formatting

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
     App\Providers\AppServiceProvider::class,
--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
 |--------------------------------------------------------------------------
 | Test Case
@@ -12,7 +14,7 @@
 */
 
 pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
 /*
