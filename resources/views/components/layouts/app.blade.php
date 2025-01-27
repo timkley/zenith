8c29fd3ce79585a1dad1ffe970931221b3630aa2
Tim Kleyersburg <tk@wacg.de>
2025-01-27 06:53:07 +0100

    feat: tailwindcss v4
    # Conflicts:
    #	package-lock.json
    #	package.json

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,8 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body>
+    <body class="font-sans m-4">
         {{ $slot }}
     </body>
 </html>
